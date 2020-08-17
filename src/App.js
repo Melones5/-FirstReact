a9e52ccbcfb593860b6128d87b17af21cc1f9343
Melones5 <alexis_32@live.com.ar>
2020-08-17 15:00:30 -0300

    componentes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MiComponente from './components/MiComponente';
+
+
+function HolaMundo(nombre, edad){
+  var presentacion = (
+      <div>
+        <h2> Hola, soy {nombre}</h2>
+        <h3> Tengo {edad} años</h3>          
+      </div>
+  );
+  return presentacion;
+}
+
 function App() {
+  var nombre = "Alexis Santos"
   return (
     <div className="App">
-      <header className="App-header">
+    <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Que onda brothelllllllll
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {HolaMundo(nombre,12)}
+
+        <section className="componente">
+          <MiComponente/>                   
+        </section>
+
       </header>
     </div>
   );
